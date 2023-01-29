49cc073dcefcf60b079ef0c4fb28ee06662a0fea
it_cwc <1766987395@qq.com>
2023-01-29 13:37:54 +0800

    配置文件调整

--- src/LLPayServiceProvider.php
+++ src/LLPayServiceProvider.php
@@ -9,7 +9,7 @@ class LLPayServiceProvider extends ServiceProvider
 
     public function register()
     {
-
+        $this->mergeConfigFrom(__DIR__.'/../config/lianlianpay.php', 'lianlianpay'); 
     }
 
     /**
@@ -18,10 +18,8 @@ class LLPayServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/config/lianlianpay.php' => config_path('lianlianpay.php'),
+            __DIR__.'/../config/lianlianpay.php' => config_path('lianlianpay.php'),
+            'config'
         ]);
     }
-
-
-
 }
