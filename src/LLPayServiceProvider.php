832d0334a018d9562e327e5dfcbbd7cdedb190f8
it_cwc <1766987395@qq.com>
2023-01-29 11:36:12 +0800

    配置文件调整

--- src/LLPayServiceProvider.php
+++ src/LLPayServiceProvider.php
@@ -6,19 +6,22 @@ use Illuminate\Support\ServiceProvider;
 
 class LLPayServiceProvider extends ServiceProvider
 {
+
+    public function register()
+    {
+
+    }
+
     /**
      * Bootstrap any application services.
      */
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/lianlianpay.php' => config_path('config/lianlianpay.php'),
+            __DIR__.'/config/lianlianpay.php' => config_path('lianlianpay.php'),
         ]);
     }
 
-    public function register()
-    {
-          
-    }
+
 
 }
